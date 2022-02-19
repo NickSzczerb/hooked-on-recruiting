d8e4d9fa79188dd3a230a48e70b3c74200dc9223
Nicholas Szczerbickyj <nick.szczerb@gmail.com>
2022-02-19 15:08:09 -0600

    completed v2 of front end file

--- front-end/home-page.py
+++ front-end/home-page.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 import streamlit as st
 from datetime import datetime
 import requests
@@ -41,6 +42,7 @@ columns1 = st.columns(2)
 title1 = columns1[0].text_input('Job Title')
 date1 = columns1[1].date_input("Start Date",
                                datetime.today(), max_value = max_date )
+date1 = date1.strftime('%Y-%m-%d %H:%m:%S')
 
 skills1 = st.multiselect('Choose your Top 3 skills',skill_list)
 
@@ -57,23 +59,70 @@ def skillsparse(value,i):
 '''###### Rate your skills'''
 columnsSkills = st.columns(3)
 
-help_text = ('''- *1-Beginner I need lots of help*
+help_text = ('''Rating Descriptions:
+- *1-Beginner I need lots of help*
 - *2-Moderate (I need some guidance)*
 - *3-Intermediate (I am independent)*
 - *4-Advanced (I have used this skill independently for +1 year)*
 - *5-Expert (I can teach others about this skill*''')
 
 
-skill_1 = columnsSkills[0].slider(f'{skillsparse(skills1,0)}', 1, 5, 2,help=help_text)
+skill_1 = columnsSkills[0].slider(f'{skillsparse(skills1,0)}', 1, 5, 3,help=help_text)
 skill_2 = columnsSkills[1].slider(f'{skillsparse(skills1,1)}',
                                   1,
                                   5,
-                                  2,
+                                  3,
                                   help=help_text)
 skill_3 = columnsSkills[2].slider(f'{skillsparse(skills1,2)}',
                                   1,
                                   5,
-                                  2,
+                                  3,
                                   help=help_text)
 
-txt1 = st.text_area('What were your responsibilities and main accomplishments in this role?')
+txt1 = st.text_area('What were your main responsibilities and accomplishments in this role?')
+
+demographics = {"email": email,
+                'name': full_name,
+                'country':country
+}
+
+
+@st.cache
+def save_data():
+    currentjob = {
+        "jobtitle1":
+        title1,
+        "startdate1":
+        date1,
+        'skills_job_1': [(skills1[0], skill_1), (skills1[1], skill_2),
+                         (skills1[2], skill_3)],
+        'job_desc_1': txt1
+    }
+    return currentjob
+
+def click_validation():
+    if len(skills1)<3:
+        return st.error("please choose 3 skills")
+    else:
+        return save_data()
+
+button1 = st.button('Click to save')
+
+if button1 and "@" not in email:
+    st.error("please enter a valid email")
+elif button1 and len(skills1) < 3:
+    st.error("please choose 3 skills")
+elif button1 and len(txt1)<100:
+    st.error(f"{len(txt1)} characters. Please fill out minimum 100 characters for your experience")
+elif button1 and len(skills1) >= 3:
+    # print is visible in the server output, not in the page
+    currentjob = click_validation()
+    st.success("data saved!")
+    """# TESTING
+    should have all datapoints in JSON"""
+    currentjob
+else:
+    st.info('Click here to save your info')
+
+#writing skills to a df
+#st.write(pd.DataFrame(currentjob.get('skills_job_1'),columns=['skills', 'rating']))
