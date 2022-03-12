1ca0c650e6702bd82ea2f10e0bea081b6b815409
Yan Bai <ryan.baiyan@gmail.com>
2022-03-12 16:27:26 -0500

    multi cell added

--- front-end/reporting_charts.py
+++ front-end/reporting_charts.py
@@ -41,7 +41,7 @@ def job_title_keyword(title_keyword_df):
     title_keyword_df=title_keyword_df.sort_values(by=title_keyword_df.columns[1], ascending=False)
     X = title_keyword_df.columns[0]
     Y = title_keyword_df.columns[1]
-    ax = title_keyword_df.plot.bar(x=X, y=Y, rot=90)
+    ax = title_keyword_df.plot.bar(x=X, y=Y, rot=45)
     ax = ax.legend(loc='best')
     return ax
 
@@ -50,44 +50,58 @@ def save_pdf(fig, wordcloud_fig, title_keyword_fig, prob):
     pdf = FPDF()  # pdf object
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    #data = [list(prob.jobs()), list(prob.values())]
-    data = [prob.jobs.tolist(), prob.iloc[:,0].tolist()]
+    prob=prob.iloc[:,[1,0,2]]
+    prob.iloc[:,1]=prob.iloc[:,1].round(3)
+    data=[]
+    for i in range(len(prob.iloc[:,0])):
+        data.append(prob.iloc[i].values.tolist())
     print(data)
 
     pdf.set_font("Times", "B", 10.0)
     epw = pdf.w - 2*pdf.l_margin
-    col_width = epw/4       
+    col_width = epw/12
     # Document title centered, 'B'old, 14 pt
     pdf.set_font('Times','B',14.0) 
-    pdf.cell(epw, 0.0, 'Demographic data', align='C')
+    pdf.cell(epw, 0.0, 'Title Prediction', align='C')
+
+    #fpdf.multi_cell(w: float, h: float, txt: str, border = 0, 
+                #align: str = 'J', fill: bool = False)
+
     pdf.set_font('Times','',10.0) 
-    pdf.ln(2*pdf.font_size)
+    pdf.ln(4*pdf.font_size)
     
     for row in data:
-        for datum in row:
+        for x,y in enumerate(row):
             # Enter data in colums
             # Notice the use of the function str to coerce any input to the 
             # string type. This is needed
             # since pyFPDF expects a string, not a number.
-            pdf.cell(col_width, pdf.font_size, str(datum), border=1)
+            if x == 0:
+                pdf.cell(col_width*3, pdf.font_size*3, str(y), border=1)
+            elif x == 1:
+                pdf.cell(col_width, pdf.font_size*3, str(y), border=1)
+            else:
+                pdf.multi_cell(col_width*8, pdf.font_size, str(y), border=1)
+
+            #pdf.multi_cell(col_width, pdf.font_size, str(datum), border=1)
         pdf.ln(pdf.font_size)  
     # Line break equivalent to 4 lines
     pdf.ln(4*pdf.font_size)     
         
     with NamedTemporaryFile(delete=True, suffix=".png") as tmpfile:
                 fig.write_image(tmpfile.name)
-                pdf.image(tmpfile.name, 5, 35, 120, 90)
+                pdf.image(tmpfile.name, 5, 105, 110, 80)
     
 
     with NamedTemporaryFile(delete=True, suffix=".png") as tmpfile:
                wordcloud_fig.savefig(tmpfile.name, dpi=wordcloud_fig.dpi)
-               pdf.image(tmpfile.name, 110, 25, 100, 110)
+               pdf.image(tmpfile.name, 110, 100, 85, 100)
 
     with NamedTemporaryFile(delete=True, suffix=".png") as tmpfile:
                #title_keyword_fig.savefig(tmpfile.name, dpi=title_keyword_fig.dpi)
-               title_keyword_fig.figure.set_size_inches(7.5, 13.5)
+               title_keyword_fig.figure.set_size_inches(6.5, 12.5)
                title_keyword_fig.figure.savefig(tmpfile.name)
-               pdf.image(tmpfile.name, 50, 135, 120, 120)
+               pdf.image(tmpfile.name, 50, 180, 110, 110)
 
     st.download_button(
         "Save as PDF",
